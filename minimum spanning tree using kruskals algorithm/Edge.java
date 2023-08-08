475619dd5972ea26a7cc21e4047901952d38c40e
Avinashs7 <117369858+Avinashs7@users.noreply.github.com>
2023-08-08 10:24:37 +0530

    Lab program

---
+++ minimum spanning tree using kruskals algorithm/Edge.java
@@ -0,0 +1,11 @@
+package kruskals;
+
+class Edge {
+	int src; int dest; int
+	weight;
+	Edge(int src, int dest, int weight) { 
+		this.src = src;
+		this.dest = dest; 
+		this.weight = weight;
+	}
+}
